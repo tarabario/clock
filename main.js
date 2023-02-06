19d5c184abf957d002536dd1c2ec23abfa097836
tarabario <mtarabario@gmail.com>
2023-02-06 12:17:12 +0200

    ugly but works

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const handSec = document.querySelector('.hand-sec');
 const handMin = document.querySelector('.hand-min');
 const handHour = document.querySelector('.hand-hour');
-const allHand = document.querySelectorAll('.hand')
-
+const allHand = document.querySelectorAll('.hand');
+const digitalClock = document.querySelector('.digital-clock');
 
 
 const ClockOn = () => {
@@ -13,17 +13,19 @@ const ClockOn = () => {
 
 	const degSec = (seconds / 60) * 360 + 90;
 	const degMin = (minutes / 60) * 360 + 90;
-	const degHour = ((hours / 12) * 360 + 90) + (1/(12 * minutes));
+	const degHour = ((hours / 12) * 360 + 90) + ((minutes / 60) * (360/12));
 
-	if (degSec === 0) {
+	if (degSec === 90) {
 		allHand.forEach(hand => hand.style.transition = 'none');
 	} else {
-		allHand.forEach(hand => hand.style.transition = 'all 0.05s');
+		allHand.forEach(hand => hand.style.transition = 'all 0.05s cubic-bezier(0, 0, 0.4, 2.39)');
 	}
 
 	handSec.style.transform = `rotate(${degSec}deg)`;
 	handMin.style.transform = `rotate(${degMin}deg)`;
 	handHour.style.transform = `rotate(${degHour}deg)`;
+
+	digitalClock.textContent = `${hours}:${minutes}:${seconds}`;
 };
 
 ClockOn();
